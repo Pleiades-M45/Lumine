9c6bdff008826b70a3f15c64cfdc1ce484d35d93
Thasneem Shahul <139751320+Pleiades-M45@users.noreply.github.com>
2025-02-19 14:58:11 +0530

    Add files via upload

---
+++ geometry/shapes3D/Sphere.java
@@ -0,0 +1,12 @@
+package geometry.shapes3D;
+import geometry.shapes2D.Circle;
+
+public class Sphere extends Circle {
+    public Sphere(double r){
+        super(r);
+    }
+
+    public double volume(){
+        return (4/3.0) * Math.PI * Math.pow(radius, 3);
+    }
+}
