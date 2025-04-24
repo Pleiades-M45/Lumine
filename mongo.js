8891d9b64636c02828859dbcb0269036faa19ecf
Thasneem Shahul <139751320+Pleiades-M45@users.noreply.github.com>
2025-04-24 01:29:54 +0000

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -27,9 +27,9 @@ mongo -u "orca" -p "123" --authenticationDatabase "abc"
 
 
 PROG  21: Create a database named "Company" in MongoDB and add a collection called Customer" with the following fields: id, name, email, phone and address.
-a)	Update the email of a customer where id is "C123".
-b)	Delete all customers who have not placed an order in the last year.
-c)	Find all customers whose name starts with "A" and have placed more than five orders.
+a)Update the email of a customer where id is "C123".
+b)Delete all customers who have not placed an order in the last year.
+c)Find all customers whose name starts with "A" and have placed more than five orders.
 
 use Company; // Create and switch to the Company database
 
@@ -46,41 +46,33 @@ db.Customer.insertMany([
 
 
 db.Customer.updateOne(
-
   { id: "C123" },
-
   { $set: { email: "alice.new@example.com" } }
-
 );
 
 
 db.Customer.deleteMany(
-
   { lastOrderDate: { $lt: new Date(new Date().setFullYear(new Date().getFullYear() - 1)) } }
-
 );
 
 
 db.Customer.find(
-
   { name: { $regex: /^A/ }, orderCount: { $gt: 5 } }
-
 );
 
 
 
 PROG  22: Create a database named "College" in MongoDB and add a collection called "Student" with the following fields: Id, Name, Age, Gender, Department, Sub1, Sub2, Attendance.
-a)	Find students who scored more than 80 in both subjects.
-b)	List students who are either in the "Computer Science" department or are older than 22.
-c)	Find students who are not in the "Electronics" department.
-d)	Retrieve students who neither scored above 80 in sub1 nor have attendance more than 85%.
-e)	Find students who are neither in "Mechanical" nor in "Civil" departments.
+a)Find students who scored more than 80 in both subjects.
+b)List students who are either in the "Computer Science" department or are older than 22.
+c)Find students who are not in the "Electronics" department.
+d)Retrieve students who neither scored above 80 in sub1 nor have attendance more than 85%.
+e)Find students who are neither in "Mechanical" nor in "Civil" departments.
   
 use College; // Create and switch to the College database
 
 // Create the Student collection and insert sample data
 db.Student.insertMany([
-
   { Id: "S001", Name: "Alice", Age: 21, Gender: "Female", Department: "Computer Science", Sub1: 85, Sub2: 90, Attendance: 88 },
 
   { Id: "S002", Name: "Bob", Age: 23, Gender: "Male", Department: "Mechanical", Sub1: 75, Sub2: 80, Attendance: 90 },
@@ -95,23 +87,17 @@ db.Student.insertMany([
 
 
 db.Student.find(
-
   { Sub1: { $gt: 80 }, Sub2: { $gt: 80 } }
-
 );
 
 
 db.Student.find(
-
   { $or: [ { Department: "Computer Science" }, { Age: { $gt: 22 } } ] }
-
 );
 
 
 db.Student.find(
-
   { Department: { $ne: "Electronics" } }
-
 );
 
 
@@ -124,9 +110,7 @@ $nor: Used for multiple conditions that must all be false. It can combine variou
 // });
 This query will return documents where neither field1 equals value1 nor field2 equals value2.
 db.Student.find(
-
   { $nor: [ { Sub1: { $gt: 80 } }, { Attendance: { $gt: 85 } } ] }
-
 );
 
 
@@ -136,7 +120,5 @@ $nin: Used for a single field to check that its value is not in a specified list
 // });
 This query will return documents where field does not have a value of value1, value2, or value3.
 db.Student.find(
-
   { Department: { $nin: ["Mechanical", "Civil"] } }
-
 );
